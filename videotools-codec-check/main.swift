2b79c54d21e4e3f323d7df85b1d610b15536306c
Leo Huber <leo_huber@icloud.com>
2022-03-19 14:56:05 +0100

    first commit

--- videotools-codec-check/main.swift
+++ videotools-codec-check/main.swift
@@ -6,6 +6,17 @@
 //
 
 import Foundation
+import VideoToolbox
 
 print("Hello, World!")
 
+var encoder_list : CFArray?
+
+VTCopyVideoEncoderList(nil, &encoder_list)
+
+var encoders = encoder_list as? Array<Any>
+
+for item in encoders ?? [] {
+    // var encoder = item as! Dictionary<AnyHashable, AnyHashable>
+    print(item)
+}
